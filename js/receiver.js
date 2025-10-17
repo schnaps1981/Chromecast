47d84bc3bc97318def0a6a765175d34a4b967e47
UlyanovAG <ulyanov@amedaiteka.ru>
2025-10-18 02:38:32 +0700

    debug logger

--- js/receiver.js
+++ js/receiver.js
@@ -2,7 +2,16 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 const options = new cast.framework.CastReceiverOptions();
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+
 options.customNamespaces = {};
+
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+    if (!castDebugLogger.debugOverlayElement_) {
+        // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+        castDebugLogger.setEnabled(true);
+    }
+});
 
 // send signal to local player that the current stream has finished
 playerManager.addEventListener(
